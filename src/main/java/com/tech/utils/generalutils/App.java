016d9c217f2a6ba1b8bb6ffeef46f1f0271f0945
razzzaque <razcoder@gmail.com>
2019-12-12 23:22:01 +0000

    update

---
+++ src/main/java/com/tech/utils/generalutils/App.java
@@ -0,0 +1,13 @@
+package com.tech.utils.generalutils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
